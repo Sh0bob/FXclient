dff5a9ad572266771b5edd09984eded9f96b73cb
peshomir <80340328+peshomir@users.noreply.github.com>
2025-12-30 21:15:52 +0200

    Improve propaganda block

--- patches/misc.js
+++ patches/misc.js
@@ -1,12 +1,16 @@
-import { definePatch } from "../modUtils.js"
+import { definePatch, insert } from "../modUtils.js"
 
-export default definePatch(({ insertCode }) => {
+export default definePatch(({ insertCode, modifyCode }) => {
 
   // Hide propaganda popup
   insertCode(`/* here */
     a = b.c + 60 * 1000;
     (new ea()).show(eS.eb, eS.colors, eS.id);
     eS = null;
-    return true;`, `if (__fx.settings.hidePropagandaPopup) return;`)
+    return true;`, `if (__fx.settings.hidePropagandaPopup || __fx.customLobby.isActive()) return;`)
+  modifyCode(`if (!a.b.c(0)) {
+			d = e.f + 1000 * 1;
+			return;
+		} ${insert(`if (!__fx.settings.hidePropagandaPopup && !__fx.customLobby.isActive())`)} a.g.h(5);`)
 
 })

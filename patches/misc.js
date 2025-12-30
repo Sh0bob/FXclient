160725594898c682a5458a78cbfc0c814d795855
peshomir <80340328+peshomir@users.noreply.github.com>
2025-12-30 21:34:41 +0200

    Automatic patches for the custom lobby version

--- patches/misc.js
+++ patches/misc.js
@@ -1,4 +1,5 @@
 import { definePatch, insert } from "../modUtils.js"
+import { styleText } from "node:util";
 
 export default definePatch(({ insertCode, modifyCode }) => {
 
@@ -13,4 +14,13 @@ export default definePatch(({ insertCode, modifyCode }) => {
 			return;
 		} ${insert(`if (!__fx.settings.hidePropagandaPopup && !__fx.customLobby.isActive())`)} a.g.h(5);`)
 
+  // for the custom lobby version
+  try {
+    modifyCode(`new a("⚔️<br>" + __L(), function() {
+      ${insert(`if (__fx.isCustomLobbyVersion) alert("This version is for use with custom lobbies only. For normal multiplayer, use the version at https://fxclient.github.io/FXclient/")
+      else`)} b(0);
+		}, ${insert(`__fx.isCustomLobbyVersion ? "rgba(50, 50, 50, 0.6)" : `)} c.d)`)
+  } catch (error) {
+    console.warn(styleText("yellow", `Warning: failed to apply patches specific to the custom lobby version`))
+  }
 })
